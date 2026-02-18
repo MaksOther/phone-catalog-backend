aaf9fe9736c771e7be7ee53a1531126ef4e15136
MaksOther <m7885091@gmail.com>
2026-02-18 18:38:29 +0200

    Add endpoint for product details

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
+app.get('/api/products/:itemId', async (req, res) => {
+  const { itemId } = req.params; 
+
+  try {
+    const { data, error } = await supabase
+      .from('product_details')
+      .select('*')
+      .eq('id', itemId)
+      .single();
+
+    if (error) {
+      return res.status(404).json({ error: "Товар не знайдено" });
+    }
+
+    res.json(data); // Віддаємо знайдений товар у React
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Сервер працює: http://localhost:${PORT}`);
